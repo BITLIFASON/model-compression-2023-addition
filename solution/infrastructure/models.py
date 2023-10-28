85d4abc8ecafb8cb4c035449f0c332591d7b9d78
BITLIFASON <vladov.ru@gmail.com>
2023-10-28 11:42:56 +0300

    onnx solution

--- solution/infrastructure/models.py
+++ solution/infrastructure/models.py
@@ -1,10 +1,11 @@
+import torch
+from optimum.onnxruntime import ORTModelForSequenceClassification
+from transformers import AutoTokenizer
+from optimum.onnxruntime import ORTQuantizer, OptimizationConfig, ORTOptimizer
+from optimum.onnxruntime.configuration import AutoQuantizationConfig
 from abc import ABC, abstractmethod
-from collections.abc import Callable
 from dataclasses import dataclass
 from typing import List
-
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 
 @dataclass
@@ -41,13 +42,13 @@ class TransformerTextClassificationModel(BaseTextClassificationModel):
 
     def tokenize_texts(self, texts: List[str]):
         inputs = self.tokenizer.batch_encode_plus(
-                texts,
-                add_special_tokens=True,
-                padding='longest',
-                truncation=True,
-                return_token_type_ids=True,
-                return_tensors='pt'
-                )
+            texts,
+            add_special_tokens=True,
+            padding='longest',
+            truncation=True,
+            return_token_type_ids=True,
+            return_tensors='pt'
+        )
         inputs = {k: v.to(self.device) for k, v in inputs.items()}  # Move inputs to GPU
         return inputs
 
@@ -57,12 +58,12 @@ class TransformerTextClassificationModel(BaseTextClassificationModel):
         label_ids = logits.argmax(dim=1)
         scores = logits.softmax(dim=-1)
         results = [
-                {
-                    "label": id2label[label_id.item()],
-                    "score": score[label_id.item()].item()
-                }
-                for label_id, score in zip(label_ids, scores)
-            ]
+            {
+                "label": id2label[label_id.item()],
+                "score": score[label_id.item()].item()
+            }
+            for label_id, score in zip(label_ids, scores)
+        ]
         return results
 
     def __call__(self, inputs) -> List[TextClassificationModelData]:
@@ -71,3 +72,42 @@ class TransformerTextClassificationModel(BaseTextClassificationModel):
         predictions = [TextClassificationModelData(self.name, **prediction) for prediction in predictions]
         return predictions
 
+
+class OptTransformerTextClassificationModel(TransformerTextClassificationModel):
+
+    def _load_model(self):
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer)
+
+        save_dir = "optimized_model" + "_" + self.name
+
+        ort_model = ORTModelForSequenceClassification.from_pretrained(
+            self.model_path,
+            export=True,
+            provider="CUDAExecutionProvider",
+        )
+
+        optimization_config = OptimizationConfig(
+            optimize_for_gpu=True,
+            optimization_level=99,
+            enable_transformers_specific_optimizations=True
+        )
+
+        optimizer = ORTOptimizer.from_pretrained(ort_model)
+
+        optimizer.optimize(save_dir=save_dir, optimization_config=optimization_config)
+
+        self.model = ORTModelForSequenceClassification.from_pretrained(save_dir)
+
+        self.model = self.model.to(self.device)
+
+    def tokenize_texts(self, texts: List[str]):
+        inputs = self.tokenizer.batch_encode_plus(
+            texts,
+            add_special_tokens=True,
+            padding='longest',
+            truncation=True,
+            return_tensors='pt',
+        )
+        inputs = {k: v.to(self.device) for k, v in inputs.items()}  # Move inputs to GPU
+        return inputs
